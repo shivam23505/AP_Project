e31f7bf881ed492dc99e007f4a22db683fb2c622
VarunGolcha20 <162430064+VarunGolcha20@users.noreply.github.com>
2024-10-18 17:21:22 +0530

    Update Structure.java

--- core/src/main/java/project_game/AP/Structure.java
+++ core/src/main/java/project_game/AP/Structure.java
@@ -8,18 +8,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Structure extends Game {
     public SpriteBatch batch;
     public BitmapFont font;
-
+    public GameScreen gameScreen;
+    public Settings settings;
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont(); // use libGDX's default Arial font
+        gameScreen=new GameScreen(this);
+        settings=new Settings(this);
         this.setScreen(new MainMenuScreen(this));
     }
-
     public void render() {
         super.render(); // important!
     }
 
     public void dispose() {
+        gameScreen.dispose();
+        settings.dispose();
         batch.dispose();
         font.dispose();
     }
