de21a51d790ae7fb9035e7d5e27e8a824ddb0727
VarunGolcha20 <162430064+VarunGolcha20@users.noreply.github.com>
2024-10-27 00:56:49 +0530

    Added assets

---
+++ assets/angrybirdstileset1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="angrybirdstileset" tilewidth="16" tileheight="16" tilecount="3600" columns="80">
+ <grid orientation="orthogonal" width="8" height="8"/>
+ <image source="OneDrive/Documents/GitHub/AP_Project/assets/background.png" width="1280" height="720"/>
+</tileset>
