ed2320f6ef6634b781dd599afaae973e1ca8f707
sjwl07 <shivam2005@gmail.com>
2024-10-27 13:50:58 +0530

    Added pixmap to leveltwo

--- core/src/main/java/project_game/AP/LevelTwo.java
+++ core/src/main/java/project_game/AP/LevelTwo.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -73,6 +70,7 @@ public class LevelTwo implements Screen {
     private Texture pig1Texture,pig2Texture,pig3Texture;
     private Texture redTexture,blackTexture;
     private Texture sling;
+    private Drawable overlayDrawable;
     public LevelTwo(Structure game) {
         this.game = game;
         background = new Texture("Level_Two_bg.jpg");
@@ -107,6 +105,9 @@ public class LevelTwo implements Screen {
 
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
+
+        Texture transparentPixel = createTransparentPixel(0f, 0f, 0f, 0.5f);
+        overlayDrawable = new TextureRegionDrawable(new TextureRegion(transparentPixel));
 
         fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Arial.ttf"));
         fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
@@ -165,11 +166,16 @@ public class LevelTwo implements Screen {
         }
     }
     public void showPauseMenu(){
-        Texture overlayImageTexture = new Texture(Gdx.files.internal("menubg3.png"));
-        Drawable overlayImageDrawable = new TextureRegionDrawable(overlayImageTexture);
-        Image overlayImage = new Image(overlayImageDrawable);
-        overlayImage.setPosition(100, 0);
-        overlayStage.addActor(overlayImage);
+//        Texture overlayImageTexture = new Texture(Gdx.files.internal("menubg3.png"));
+//        Drawable overlayImageDrawable = new TextureRegionDrawable(overlayImageTexture);
+//        Image overlayImage = new Image(overlayImageDrawable);
+//        overlayImage.setPosition(100, 0);
+//        overlayStage.addActor(overlayImage);
+
+        Table backgroundTable = new Table();
+        backgroundTable.setFillParent(true);
+        backgroundTable.setBackground(overlayDrawable);  // Set the transparent background
+        overlayStage.addActor(backgroundTable);
 
         TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(new Texture("continue_button.png")));
         ImageButton.ImageButtonStyle buttonStyle = new ImageButton.ImageButtonStyle();
@@ -187,6 +193,19 @@ public class LevelTwo implements Screen {
             }
         });
 //        overlayStage.addActor(continueButton);
+
+        drawable = new TextureRegionDrawable(new TextureRegion(new Texture("menu_save_btn.png")));
+        ImageButton.ImageButtonStyle buttonStyle3 = new ImageButton.ImageButtonStyle();
+        buttonStyle3.imageUp = drawable;
+        ImageButton saveButton = new ImageButton(buttonStyle3);
+        saveButton.setSize(90, 90);
+
+        saveButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Clicked! GAME STATE SAVED!!");
+            }
+        });
 
         drawable = new TextureRegionDrawable(new TextureRegion(new Texture("exit_button.png")));
         ImageButton.ImageButtonStyle buttonStyle2 = new ImageButton.ImageButtonStyle();
@@ -213,15 +232,24 @@ public class LevelTwo implements Screen {
         table2.setFillParent(true);
 //        table2.center();
 //        table2.row();
-        table2.add(label).padLeft(40).padTop(-90);
+        table2.add(label).padLeft(40).padTop(-10);
         table2.row();
         table2.add(continueButton).padTop(65).padLeft(40).padBottom(25);
+        table2.row();
+        table2.add(saveButton).center().padLeft(40).padBottom(25);
         table2.row();
         table2.add(exitButton).center().padLeft(40);
 
         overlayStage.addActor(table2);
     }
-
+    private Texture createTransparentPixel(float r, float g, float b, float alpha) {
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(r, g, b, alpha);  // Set RGBA for transparency
+        pixmap.fill();
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
+        return texture;
+    }
     @Override
     public void show() {
 
@@ -265,6 +293,7 @@ public class LevelTwo implements Screen {
         }
         else{
             Gdx.input.setInputProcessor(stage);
+            overlayStage.clear();
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
             if (!background_changedw) {
